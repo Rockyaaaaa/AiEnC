f2b77885ec655de6fba4121387e106589b9ca3cd
Rocky <coolxin@live.cn>
2017-07-27 22:32:49 +0800

    Set Spinner

--- app/src/main/java/com/rocky/aienc/ScreenSlidePageFragment.java
+++ app/src/main/java/com/rocky/aienc/ScreenSlidePageFragment.java
@@ -1,8 +1,6 @@
 package com.rocky.aienc;
 
-/**
- * Created by zhengdan on 2017/7/6.
- */
+
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -16,63 +14,84 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ScreenSlidePageFragment extends Fragment implements
-    AdapterView.OnItemSelectedListener {
+public class ScreenSlidePageFragment extends Fragment {
 
-  private EditText originalNum;
-  private TextView convertedNum;
+    private EditText originalNum;
+    private TextView convertedNum;
+    ArrayAdapter<CharSequence> unitAdapter;
+    Spinner unitSpinner;
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+        originalNum = (EditText) rootView.findViewById(R.id.originalNum);
+        convertedNum = (TextView) rootView.findViewById(R.id.convertedNum);
+
+        Spinner unitTypeSpinner = (Spinner) rootView.findViewById(R.id.unit_type_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> unitTypeAdapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.unit_type, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        unitTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        unitTypeSpinner.setAdapter(unitTypeAdapter);
+
+        unitSpinner = (Spinner) rootView.findViewById(R.id.unit_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        setUnitAdapter(R.array.length_unit_type);
+
+        originalNum.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                convertedNum.setText(v.getText());
+                return false;
+            }
+        });
+
+        unitTypeSpinner.setSelection(0, true);
+        unitTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        setUnitAdapter(R.array.length_unit_type);
+                        break;
+                    case 1:
+                        setUnitAdapter(R.array.area_unit_type);
+                        break;
+                    case 2:
+                        setUnitAdapter(R.array.pressure_unit_type);
+                        break;
+                    case 3:
+                        setUnitAdapter(R.array.flow_unit_type);
+                        break;
+                    case 4:
+                        setUnitAdapter(R.array.temperature_unit_type);
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return rootView;
+    }
 
 
-    ViewGroup rootView = (ViewGroup) inflater.inflate(
-        R.layout.fragment_screen_slide_page, container, false);
-    originalNum = (EditText) rootView.findViewById(R.id.originalNum);
-    convertedNum = (TextView) rootView.findViewById(R.id.convertedNum);
-
-    Spinner unitTypeSpinner = (Spinner) rootView.findViewById(R.id.unit_type_spinner);
-    // Create an ArrayAdapter using the string array and a default spinner layout
-    ArrayAdapter<CharSequence> unitTypeAdapter = ArrayAdapter.createFromResource(this.getContext(),
-        R.array.unit_type, android.R.layout.simple_spinner_item);
-    // Specify the layout to use when the list of choices appears
-    unitTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
-    unitTypeSpinner.setAdapter(unitTypeAdapter);
-
-
-    Spinner unitSpinner = (Spinner) rootView.findViewById(R.id.unit_spinner);
-    // Create an ArrayAdapter using the string array and a default spinner layout
-    ArrayAdapter<CharSequence> unitAdapter = ArrayAdapter.createFromResource(this.getContext(),
-        R.array.length_unit_type, android.R.layout.simple_spinner_item);
-    // Specify the layout to use when the list of choices appears
-    unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
-    unitSpinner.setAdapter(unitAdapter);
-
-    originalNum.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-      @Override public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        convertedNum.setText(v.getText());
-        return false;
-      }
-    });
-    return rootView;
-  }
-
-  @Override
-  public void onItemSelected(AdapterView<?> parent, View view,
-      int pos, long id) {
-    // An item was selected. You can retrieve the selected item using
-    // parent.getItemAtPosition(pos)
-
-
-  }
-
-  @Override
-  public void onNothingSelected(AdapterView<?> parent) {
-    // Another interface callback
-  }
+    public void setUnitAdapter(int i) {
+        unitAdapter = ArrayAdapter.createFromResource(this.getContext(),
+                i, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        unitSpinner.setAdapter(unitAdapter);
+    }
 
 
 }
