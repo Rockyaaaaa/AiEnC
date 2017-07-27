efdac3e2c7888bec1a96d938a1f3eab5d195c0fe
Rocky <coolxin@live.cn>
2017-07-27 23:54:58 +0800

    Add ListView

--- app/src/main/java/com/rocky/aienc/ScreenSlidePageFragment.java
+++ app/src/main/java/com/rocky/aienc/ScreenSlidePageFragment.java
@@ -2,22 +2,29 @@ package com.rocky.aienc;
 
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScreenSlidePageFragment extends Fragment {
 
     private EditText originalNum;
-    private TextView convertedNum;
+    private ListView convertedNum;
     ArrayAdapter<CharSequence> unitAdapter;
     Spinner unitSpinner;
 
@@ -28,7 +35,7 @@ public class ScreenSlidePageFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
         originalNum = (EditText) rootView.findViewById(R.id.originalNum);
-        convertedNum = (TextView) rootView.findViewById(R.id.convertedNum);
+        convertedNum = (ListView) rootView.findViewById(R.id.convertedNum);
 
         Spinner unitTypeSpinner = (Spinner) rootView.findViewById(R.id.unit_type_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -43,15 +50,24 @@ public class ScreenSlidePageFragment extends Fragment {
         // Create an ArrayAdapter using the string array and a default spinner layout
         setUnitAdapter(R.array.length_unit_type);
 
+        final ArrayAdapter<String> convertNumAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_expandable_list_item_1, getData());
+        convertedNum.setAdapter(convertNumAdapter);
+
+
         originalNum.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                convertedNum.setText(v.getText());
-                return false;
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    handled = true;
+                    ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE))
+                            .hideSoftInputFromWindow(originalNum.getWindowToken(), 0);
+                }
+                return handled;
             }
         });
 
-        unitTypeSpinner.setSelection(0, true);
         unitTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -93,5 +109,14 @@ public class ScreenSlidePageFragment extends Fragment {
         unitSpinner.setAdapter(unitAdapter);
     }
 
+    private List<String> getData(){
 
+        List<String> data = new ArrayList<>();
+        data.add("测试数据1");
+        data.add("测试数据2");
+        data.add("测试数据3");
+        data.add("测试数据4");
+
+        return data;
+    }
 }
